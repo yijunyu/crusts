7f0cac308ff3992131d2a3dda2184485530f3cbe
Yijun Yu <y.yu@open.ac.uk>
2022-11-27 23:03:03 +0000

    integrate with c2rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::{
-    process::{Command, Stdio}, path::Path,
-};
 use jwalk::WalkDir;
-
+use std::{
+    path::Path,
+    process::{Command, Stdio},
+};
 pub fn is_file_with_ext(p: &Path, file_ext: &str) -> bool {
     let ext = match p.extension() {
         Some(e) => e,
@@ -12,11 +12,60 @@ pub fn is_file_with_ext(p: &Path, file_ext: &str) -> bool {
 }
 
 fn main() {
-    WalkDir::new(".").sort(true).into_iter().for_each(|entry| {
+    if !std::path::Path::new("compile_commands.json").exists()
+        & &(std::path::Path::new("Makefile").exists() || std::path::Path::new("Makefile").exists())
+    {
+        if let Ok(command) = Command::new("intercept-build")
+            .args(["make"])
+            .stdout(Stdio::piped())
+            .spawn()
+        {
+            if let Ok(output) = command.wait_with_output() {
+                println!("{:?}", output);
+            }
+        }
+    }
+    match Command::new("c2rust-transpile")
+        .args(["-e", "-b", "main", "-o", "c2rust", "compile_commands.json"])
+        .stdout(Stdio::piped())
+        .spawn()
+    {
+        Ok(command) => {
+            if let Ok(output) = command.wait_with_output() {
+                println!("{:?}", output);
+            }
+        }
+        Err(_) => {
+            Command::new("cargo")
+                .args(["install", "c2rust-transpile"])
+                .stdout(Stdio::piped())
+                .spawn()
+                .ok();
+        }
+    }
+    match Command::new("c2rust-transpile")
+        .args(["-e", "-b", "main", "-o", "crusts", "compile_commands.json"])
+        .stdout(Stdio::piped())
+        .spawn()
+    {
+        Ok(command) => {
+            if let Ok(output) = command.wait_with_output() {
+                println!("{:?}", output);
+            }
+        }
+        Err(_) => {
+            Command::new("cargo")
+                .args(["install", "c2rust-transpile"])
+                .stdout(Stdio::piped())
+                .spawn()
+                .ok();
+        }
+    }
+    WalkDir::new("./crusts").sort(true).into_iter().for_each(|entry| {
         if let Ok(e) = entry {
             let p = e.path();
             if !is_file_with_ext(&p, "rs") {
-                return; 
+                return;
             }
             let file = &format!("{}", &p.into_os_string().to_string_lossy());
             fix_unsafe(file);
@@ -62,28 +111,69 @@ fn fix_unsafe(file: &str) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::process::Command;
     #[test]
     fn test_main() {
         let dir = std::path::Path::new("abc");
-        if !dir.exists() {
-            Command::new("cargo")
-                .args(["init", "--bin", "abc"])
-                .spawn()
-                .ok();
-            let code = r#"
-unsafe fn main() {
-    println!("Hello, world!");
-}
-"#;
-            std::fs::write("abc/src/main.rs", code).ok();
+        if dir.exists() {
+            std::fs::remove_dir_all(dir).ok();
         }
+        std::fs::create_dir_all(dir).ok();
+        std::fs::write(
+            "abc/main.c",
+            r#"
+#include <stdio.h>
+int main() {
+    printf("Hello, world!");
+    return 0;
+}
+"#,
+        )
+        .ok();
+        std::fs::write("abc/Makefile", "main: main.c\n\tgcc -o main main.c\n").ok();
         std::env::set_current_dir(dir).ok();
         main();
-        if let Ok(s) = std::fs::read_to_string("src/main.rs") {
+        if let Ok(s) = std::fs::read_to_string("c2rust/src/main.rs") {
             insta :: assert_snapshot! (s, @ r###"
-            fn main() {
-                println!("Hello, world!");
+            #![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_assignments, unused_mut)]
+            #![register_tool(c2rust)]
+            #![feature(register_tool)]
+            use ::c2rust::*;
+            extern "C" {
+                fn printf(_: *const libc::c_char, _: ...) -> libc::c_int;
+            }
+            unsafe fn main_0() -> libc::c_int {
+                printf(b"Hello, world!\0" as *const u8 as *const libc::c_char);
+                return 0 as libc::c_int;
+            }
+            pub fn main() {
+                unsafe { ::std::process::exit(main_0() as i32) }
+            }
+            "###);
+        }
+        if let Ok(s) = std::fs::read_to_string("crusts/src/main.rs") {
+            insta :: assert_snapshot! (s, @ r###"
+            #![allow(
+                dead_code,
+                mutable_transmutes,
+                non_camel_case_types,
+                non_snake_case,
+                non_upper_case_globals,
+                unused_assignments,
+                unused_mut
+            )]
+            #![register_tool(c2rust)]
+            #![feature(register_tool)]
+            use crusts::*;
+            extern "C" {
+                fn printf(_: *const libc::c_char, _: ...) -> libc::c_int;
+            }
+            fn main_0() -> libc::c_int {
+                printf(b"Hello, world!\0" as *const u8 as *const libc::c_char);
+                return 0 as libc::c_int;
+            }
+
+            pub fn main() {
+                ::std::process::exit(main_0() as i32);
             }
             "###);
         }
