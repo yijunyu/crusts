37fd6015fa777ad74d24985fdc0bf34b01e1b6e2
Yijun Yu <y.yu@open.ac.uk>
2022-12-25 11:24:16 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -15,97 +15,81 @@ pub fn is_file_with_ext(p: &Path, file_ext: &str) -> bool {
 }
 
 fn main() {
-    if !std::path::Path::new("compile_commands.json").exists()
-    {
-        if !std::path::Path::new("Makefile").exists() && !std::path::Path::new("makefile").exists() && ! std::path::Path::new("configure").exists() && ! std::path::Path::new("configure.ac").exists()  {
-            let mut c_files = Vec::new();
-            WalkDir::new(".").sort(true).into_iter().for_each(|entry| {
-                if let Ok(e) = entry {
-                    let p = e.path();
-                    if !is_file_with_ext(&p, "c") && !is_file_with_ext(&p, "cpp") {
-                        return;
+    if !std::path::Path::new("Cargo.toml").exists() { // This is a C project
+        if !std::path::Path::new("compile_commands.json").exists()
+        {
+            if !std::path::Path::new("Makefile").exists() && !std::path::Path::new("makefile").exists() && ! std::path::Path::new("configure").exists() && ! std::path::Path::new("configure.ac").exists()  {
+                let mut c_files = Vec::new();
+                WalkDir::new(".").sort(true).into_iter().for_each(|entry| {
+                    if let Ok(e) = entry {
+                        let p = e.path();
+                        if !is_file_with_ext(&p, "c") && !is_file_with_ext(&p, "cpp") {
+                            return;
+                        }
+                        let file = format!("{}", &p.into_os_string().to_string_lossy());
+                        c_files.push(file);
                     }
-                    let file = format!("{}", &p.into_os_string().to_string_lossy());
-                    c_files.push(file);
+                });
+                let mut obj = "".to_string();
+                for c_file in c_files {
+                    obj.push_str(" \\\n");
+                    obj.push_str(&c_file.replace(".c", ".o"));
                 }
-            });
-            let mut obj = "".to_string();
-            for c_file in c_files {
-                obj.push_str(" \\\n");
-                obj.push_str(&c_file.replace(".c", ".o"));
+                std::fs::write("Makefile", format!("main: {}\n\tgcc -o main {}\n\n.c.o: \n\tgcc -c $<\n\n.cpp.o: \n\tg++ -c $<\n\nclean::\n\trm -rf Makefile main c2rust crusts compile_commands.json txl10.8b.linux64", obj, obj)).ok();
             }
-            std::fs::write("Makefile", format!("main: {}\n\tgcc -o main {}\n\n.c.o: \n\tgcc -c $<\n\n.cpp.o: \n\tg++ -c $<\n\nclean::\n\trm -rf Makefile main c2rust crusts compile_commands.json txl10.8b.linux64", obj, obj)).ok();
+            if !std::path::Path::new("Makefile").exists() && !std::path::Path::new("configure").exists() && std::path::Path::new("configure.ac").exists()  {
+                if let Ok(command) = Command::new("autoreconf")
+                    .args(["-fi"])
+                    .stdout(Stdio::piped())
+                    .spawn()
+                {
+                    if let Ok(output) = command.wait_with_output() {
+                        println!("{:?}", output);
+                    }
+                }
+            }
+            if !std::path::Path::new("Makefile").exists() && std::path::Path::new("configure").exists()  {
+                if let Ok(command) = Command::new("./configure")
+                    .stdout(Stdio::piped())
+                    .spawn()
+                {
+                    if let Ok(output) = command.wait_with_output() {
+                        println!("{:?}", output);
+                    }
+                }
+            }
+            if std::path::Path::new("Makefile").exists() {
+                if let Ok(command) = Command::new("intercept-build")
+                    .args(["make", "-k"])
+                    .stdout(Stdio::piped())
+                    .spawn()
+                {
+                    if let Ok(output) = command.wait_with_output() {
+                        println!("{:?}", output);
+                    }
+                }
+            }
         }
-        if !std::path::Path::new("Makefile").exists() && !std::path::Path::new("configure").exists() && std::path::Path::new("configure.ac").exists()  {
-            if let Ok(command) = Command::new("autoreconf")
-                .args(["-fi"])
-                .stdout(Stdio::piped())
-                .spawn()
-            {
+        match Command::new("c2rust-transpile")
+            .args(["-e", "-b", "main", "-o", ".", "compile_commands.json"])
+            .stdout(Stdio::piped())
+            .spawn()
+        {
+            Ok(command) => {
                 if let Ok(output) = command.wait_with_output() {
                     println!("{:?}", output);
                 }
             }
-        }
-        if !std::path::Path::new("Makefile").exists() && std::path::Path::new("configure").exists()  {
-            if let Ok(command) = Command::new("./configure")
-                .stdout(Stdio::piped())
-                .spawn()
-            {
-                if let Ok(output) = command.wait_with_output() {
-                    println!("{:?}", output);
-                }
-            }
-        }
-        if std::path::Path::new("Makefile").exists() {
-            if let Ok(command) = Command::new("intercept-build")
-                .args(["make", "-k"])
-                .stdout(Stdio::piped())
-                .spawn()
-            {
-                if let Ok(output) = command.wait_with_output() {
-                    println!("{:?}", output);
-                }
+            Err(_) => {
+                Command::new("cargo")
+                    .args(["install", "c2rust-transpile"])
+                    .stdout(Stdio::piped())
+                    .spawn()
+                    .ok();
             }
         }
     }
-    match Command::new("c2rust-transpile")
-        .args(["-e", "-b", "main", "-o", "c2rust", "compile_commands.json"])
-        .stdout(Stdio::piped())
-        .spawn()
-    {
-        Ok(command) => {
-            if let Ok(output) = command.wait_with_output() {
-                println!("{:?}", output);
-            }
-        }
-        Err(_) => {
-            Command::new("cargo")
-                .args(["install", "c2rust-transpile"])
-                .stdout(Stdio::piped())
-                .spawn()
-                .ok();
-        }
-    }
-    match Command::new("c2rust-transpile")
-        .args(["-e", "-b", "main", "-o", "crusts", "compile_commands.json"])
-        .stdout(Stdio::piped())
-        .spawn()
-    {
-        Ok(command) => {
-            if let Ok(output) = command.wait_with_output() {
-                println!("{:?}", output);
-            }
-        }
-        Err(_) => {
-            Command::new("cargo")
-                .args(["install", "c2rust-transpile"])
-                .stdout(Stdio::piped())
-                .spawn()
-                .ok();
-        }
-    }
-    WalkDir::new("./crusts").sort(true).into_iter().for_each(|entry| {
+    WalkDir::new(".").sort(true).into_iter().for_each(|entry| {
         if let Ok(e) = entry {
             let p = e.path();
             if !is_file_with_ext(&p, "rs") {
@@ -213,10 +197,10 @@ int main() {
 "#,
         )
         .ok();
-        std::fs::write("abc/Makefile", "main: main.c\n\tgcc -o main main.c\n\nclean::\n\trm -rf main c2rust crusts compile_commands.json txl10.8b.linux64").ok();
+        std::fs::write("abc/Makefile", "main: main.c\n\tgcc -o main main.c\n\nclean::\n\trm -rf main compile_commands.json src Cargo.toml *.rs rust-toolchain txl10.8b.linux64").ok();
         std::env::set_current_dir(dir).ok();
         main();
-        if let Ok(s) = std::fs::read_to_string("c2rust/src/main.rs") {
+        if let Ok(s) = std::fs::read_to_string("src/main.rs") {
             insta :: assert_snapshot! (s, @ r###"
             #![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_assignments, unused_mut)]
             #![register_tool(c2rust)]
@@ -233,50 +217,6 @@ int main() {
                 unsafe { ::std::process::exit(main_0() as i32) }
             }
             "###);
-        }
-        if let Ok(s) = std::fs::read_to_string("crusts/src/main.rs") {
-            insta :: assert_snapshot! (s, @ r###"
-            #![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_assignments, unused_mut)]
-            #![register_tool(c2rust)]
-            #![feature(register_tool)]
-            use ::crusts::*;
-            extern "C" {
-                fn printf(_: *const libc::c_char, _: ...) -> libc::c_int;
-            }
-            unsafe fn main_0() -> libc::c_int {
-                printf(b"Hello, world!\0" as *const u8 as *const libc::c_char);
-                return 0 as libc::c_int;
-            }
-            pub fn main() {
-                unsafe { ::std::process::exit(main_0() as i32) }
-            }
-            "###);
-        }
-    }
-    #[test]
-    fn test_automake() {
-        let dir = std::path::Path::new("bench/sigx");
-        if dir.exists() {
-            std::env::set_current_dir(dir).ok();
-            main();
-            if let Ok(s) = std::fs::read_to_string("c2rust/src/main.rs") {
-                insta :: assert_snapshot! (s, @ r###"
-                #![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_assignments, unused_mut)]
-                #![register_tool(c2rust)]
-                #![feature(register_tool)]
-                use ::c2rust::*;
-                extern "C" {
-                    fn printf(_: *const libc::c_char, _: ...) -> libc::c_int;
-                }
-                unsafe fn main_0() -> libc::c_int {
-                    printf(b"Hello, world!\0" as *const u8 as *const libc::c_char);
-                    return 0 as libc::c_int;
-                }
-                pub fn main() {
-                    unsafe { ::std::process::exit(main_0() as i32) }
-                }
-                "###);
-            }
         }
     }
 }
