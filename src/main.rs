2646142cd9d76fd1a35d4aef070eef9557c0e6b9
Yijun Yu <y.yu@open.ac.uk>
2022-12-26 09:14:50 +0000

    fix environment variable of PATH

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
+use flate2::read::GzDecoder;
 use jwalk::WalkDir;
 use std::{
     path::Path,
     process::{Command, Stdio},
 };
-use flate2::read::GzDecoder;
 use tar::Archive;
-use uuid::Uuid;
 pub fn is_file_with_ext(p: &Path, file_ext: &str) -> bool {
     let ext = match p.extension() {
         Some(e) => e,
@@ -15,10 +14,14 @@ pub fn is_file_with_ext(p: &Path, file_ext: &str) -> bool {
 }
 
 fn main() {
-    if !std::path::Path::new("Cargo.toml").exists() { // This is a C project
-        if !std::path::Path::new("compile_commands.json").exists()
-        {
-            if !std::path::Path::new("Makefile").exists() && !std::path::Path::new("makefile").exists() && ! std::path::Path::new("configure").exists() && ! std::path::Path::new("configure.ac").exists()  {
+    if !std::path::Path::new("Cargo.toml").exists() {
+        // This is a C project
+        if !std::path::Path::new("compile_commands.json").exists() {
+            if !std::path::Path::new("Makefile").exists()
+                && !std::path::Path::new("makefile").exists()
+                && !std::path::Path::new("configure").exists()
+                && !std::path::Path::new("configure.ac").exists()
+            {
                 let mut c_files = Vec::new();
                 WalkDir::new(".").sort(true).into_iter().for_each(|entry| {
                     if let Ok(e) = entry {
@@ -35,9 +38,12 @@ fn main() {
                     obj.push_str(" \\\n");
                     obj.push_str(&c_file.replace(".c", ".o"));
                 }
-                std::fs::write("Makefile", format!("main: {}\n\tgcc -o main {}\n\n.c.o: \n\tgcc -c $<\n\n.cpp.o: \n\tg++ -c $<\n\nclean::\n\trm -rf Makefile main c2rust crusts compile_commands.json txl10.8b.linux64", obj, obj)).ok();
+                std::fs::write("Makefile", format!("main: {}\n\tgcc -o main {}\n\n.c.o: \n\tgcc -c $<\n\n.cpp.o: \n\tg++ -c $<\n\nclean::\n\trm -rf Makefile main c2rust crusts compile_commands.json txl10.8b.linux64 Cargo.lock target", obj, obj)).ok();
             }
-            if !std::path::Path::new("Makefile").exists() && !std::path::Path::new("configure").exists() && std::path::Path::new("configure.ac").exists()  {
+            if !std::path::Path::new("Makefile").exists()
+                && !std::path::Path::new("configure").exists()
+                && std::path::Path::new("configure.ac").exists()
+            {
                 if let Ok(command) = Command::new("autoreconf")
                     .args(["-fi"])
                     .stdout(Stdio::piped())
@@ -48,11 +54,10 @@ fn main() {
                     }
                 }
             }
-            if !std::path::Path::new("Makefile").exists() && std::path::Path::new("configure").exists()  {
-                if let Ok(command) = Command::new("./configure")
-                    .stdout(Stdio::piped())
-                    .spawn()
-                {
+            if !std::path::Path::new("Makefile").exists()
+                && std::path::Path::new("configure").exists()
+            {
+                if let Ok(command) = Command::new("./configure").stdout(Stdio::piped()).spawn() {
                     if let Ok(output) = command.wait_with_output() {
                         println!("{:?}", output);
                     }
@@ -89,16 +94,7 @@ fn main() {
             }
         }
     }
-    WalkDir::new(".").sort(true).into_iter().for_each(|entry| {
-        if let Ok(e) = entry {
-            let p = e.path();
-            if !is_file_with_ext(&p, "rs") {
-                return;
-            }
-            let file = &format!("{}", &p.into_os_string().to_string_lossy());
-            crusts(file);
-        }
-    });
+    crusts();
 }
 
 extern crate reqwest;
@@ -116,17 +112,26 @@ const FOLDER: &str = "Txl108bwin64";
 #[cfg(target_os = "windows")]
 const URL: &str = "http://bertrust.s3.amazonaws.com/crusts-windows.tar.gz";
 
-fn crusts(file: &str) {
+fn crusts() {
     if !std::path::Path::new(&format!("{}/lib/Rust/unsafe.x", FOLDER)).exists() {
+        println!("downloading ... ");
+        std::fs::create_dir(&format!("{}/lib/Rust", FOLDER)).ok();
         if let Ok(resp) = reqwest::blocking::get(URL) {
             if let Ok(bytes) = resp.bytes() {
-               let tar = GzDecoder::new(&bytes[..]);
-               let mut archive = Archive::new(tar);
-               archive.unpack(format!("{}/lib", FOLDER)).ok();
+                let tar = GzDecoder::new(&bytes[..]);
+                let mut archive = Archive::new(tar);
+                archive.unpack(format!("{}/lib", FOLDER)).ok();
+            } else {
+                eprintln!("Couldn't download, please check your network connection.");
             }
+            println!("downloaded ... ");
+        } else {
+            eprintln!("Couldn't download, please check your network connection.");
+            return;
         }
     }
-    let rules = vec!["formalizeCode.x",
+    let rules = vec![
+        "formalizeCode.x",
         "varTypeNoBounds.x",
         "null.x",
         "array.x",
@@ -135,45 +140,42 @@ fn crusts(file: &str) {
         "atoi.x",
         "time.x",
         "const2mut.x",
-        "main.x",
-        "stdio.x",
+        // "main.x",
+        // "stdio.x",
         "unsafe.x",
-       ];
-    std::env::set_var("PATH", format!("{}/lib/Rust", FOLDER));
+    ];
+    std::env::set_var("PATH", format!("{}/lib/Rust:{:?}", FOLDER, std::env::var("PATH")));
     std::env::set_var("txl_rules", format!("{}/lib/Rust", FOLDER));
-    let uuid = format!("{:?}.rs", Uuid::new_v4());
     for r in rules {
-        let args = vec![
-            "-q".to_string(),
-            "-s".to_string(),
-            "3000".to_string(),
-            file.to_string(),
-            format!("{}/lib/Rust/{}", FOLDER, r.to_string()),
-        ];
-        // if let Ok(output) = txl_rs::txl(args) {
-        if let Ok(txl_command) = Command::new(r)
-            .args(args)
-            .stdout(Stdio::piped())
-            .spawn() {
-            if let Ok(output) = txl_command.wait_with_output() {
-                std::fs::write(&uuid, format!("{:?}", output).as_bytes().as_ref()).ok();
-                if let Ok(command) = Command::new("rustfmt")
-                    .args([&uuid])
-                    .stdout(Stdio::piped())
-                    .spawn()
-                {
-                    if let Ok(_output) = command.wait_with_output() {
-                        if let Ok(s) = std::fs::read_to_string(file) {
-                            std::fs::write(file, s).ok();
-                        }
+        println!("applying {r}...");
+        WalkDir::new(".").sort(true).into_iter().for_each(|entry| {
+            if let Ok(e) = entry {
+                let p = e.path();
+                if !is_file_with_ext(&p, "rs") {
+                    // println!("ignoring {:?}", &p);
+                    return;
+                }
+                let file = &format!("{}", &p.into_os_string().to_string_lossy());
+                let args = vec![
+                    file.to_string(),
+                    "-o".to_string(),
+                    file.to_string(),
+                ];
+                if let Ok(txl_command) = Command::new(format!("{}/lib/Rust/{}", FOLDER, r.to_string())).args(args).spawn() {
+                    if let Ok(_output) = txl_command.wait_with_output() {
+                        let _ = match Command::new("rustfmt").args([&file]).spawn() {
+                            Ok(mut rustfmt_command) =>  { rustfmt_command.wait().unwrap(); },
+                            Err(e) => { eprintln!("rustfmt {e}: {file} has got a problem!")} ,
+                        };
+                    } else {
+                        eprintln!("txl {r} has got a problem!");
                     }
+                } else {
+                    eprintln!("{r} has got a problem!");
                 }
             }
-        } else {
-            eprintln!("{r} has got a problem!");
-        }
+        });
     }
-    std::fs::remove_file(uuid).ok();
 }
 
 #[cfg(test)]
@@ -202,19 +204,30 @@ int main() {
         main();
         if let Ok(s) = std::fs::read_to_string("src/main.rs") {
             insta :: assert_snapshot! (s, @ r###"
-            #![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_assignments, unused_mut)]
+            #![allow(
+                dead_code,
+                mutable_transmutes,
+                non_camel_case_types,
+                non_snake_case,
+                non_upper_case_globals,
+                unused_assignments,
+                unused_mut
+            )]
             #![register_tool(c2rust)]
             #![feature(register_tool)]
-            use ::c2rust::*;
+            use c2rust_out::*;
             extern "C" {
-                fn printf(_: *const libc::c_char, _: ...) -> libc::c_int;
+                fn printf(_: *const i8, _: ...) -> i32;
             }
-            unsafe fn main_0() -> libc::c_int {
-                printf(b"Hello, world!\0" as *const u8 as *const libc::c_char);
-                return 0 as libc::c_int;
+            fn main_0() -> i32 {
+                unsafe {
+                    printf(b"Hello, world!\0" as *const u8 as *const i8);
+                }
+                return 0;
             }
+
             pub fn main() {
-                unsafe { ::std::process::exit(main_0() as i32) }
+                ::std::process::exit(main_0() as i32);
             }
             "###);
         }
